049da08e82630153208259b2d7c9cdac101c9862
ZipCPU <dgisselq@ieee.org>
2018-05-17 19:56:45 -0400

    Fixed some formal configuration issues

--- rtl/wbarbiter.v
+++ rtl/wbarbiter.v
@@ -16,7 +16,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (C) 2015,2017, Gisselquist Technology, LLC
+// Copyright (C) 2015-2018, Gisselquist Technology, LLC
 //
 // This program is free software (firmware): you can redistribute it and/or
 // modify it under the terms of  the GNU General Public License as published
@@ -52,12 +52,22 @@ module	wbarbiter(i_clk, i_reset,
 	i_b_cyc, i_b_stb, i_b_we, i_b_adr, i_b_dat, i_b_sel,
 		o_b_ack, o_b_stall, o_b_err,
 	// Combined/arbitrated bus
-	o_cyc, o_stb, o_we, o_adr, o_dat, o_sel, i_ack, i_stall, i_err);
+	o_cyc, o_stb, o_we, o_adr, o_dat, o_sel, i_ack, i_stall, i_err
+`ifdef	FORMAL
+	,
+	f_a_nreqs, f_a_nacks, f_a_outstanding,
+	f_b_nreqs, f_b_nacks, f_b_outstanding,
+	f_nreqs,   f_nacks,   f_outstanding
+`endif
+	);
 	parameter			DW=32, AW=32;
 	parameter			SCHEME="ALTERNATING";
 	parameter	[0:0]		OPT_ZERO_ON_IDLE = 1'b0;
+	parameter			F_MAX_STALL = 3;
+	parameter			F_MAX_ACK_DELAY = 3;
 `ifdef	FORMAL
 	parameter			F_LGDEPTH=3;
+	parameter			F_OPT_CLK2FFLOGIC=1'b0;
 `endif
 
 	//
@@ -80,6 +90,11 @@ module	wbarbiter(i_clk, i_reset,
 	output	wire	[(DW-1):0]	o_dat;
 	output	wire	[(DW/8-1):0]	o_sel;
 	input	wire			i_ack, i_stall, i_err;
+	//
+	output	wire	[(F_LGDEPTH-1):0] f_nreqs, f_nacks, f_outstanding,
+			f_a_nreqs, f_a_nacks, f_a_outstanding,
+			f_b_nreqs, f_b_nacks, f_b_outstanding;
+
 
 	// Go high immediately (new cycle) if ...
 	//	Previous cycle was low and *someone* is requesting a bus cycle
@@ -87,8 +102,9 @@ module	wbarbiter(i_clk, i_reset,
 	//	We were just high and the owner no longer wants the bus
 	// WISHBONE Spec recommends no logic between a FF and the o_cyc
 	//	This violates that spec.  (Rec 3.15, p35)
-	assign o_cyc = (r_a_owner) ? i_a_cyc : i_b_cyc;
 	reg	r_a_owner;
+
+	assign o_cyc = (r_a_owner) ? i_a_cyc : i_b_cyc;
 	initial	r_a_owner = 1'b1;
 
 	generate if (SCHEME == "PRIORITY")
@@ -201,31 +217,39 @@ module	wbarbiter(i_clk, i_reset,
 `ifdef	FORMAL
 
 `ifdef	WBARBITER
-	reg	f_last_clk;
-	initial	assume(!i_clk);
-	always @($global_clock)
+	generate if (F_OPT_CLK2FFLOGIC)
 	begin
-		assume(i_clk != f_last_clk);
-		f_last_clk <= i_clk;
-	end
-`define	`ASSUME	assume
+		reg	f_last_clk;
+		initial	assume(!i_clk);
+		always @($global_clock)
+		begin
+			assume(i_clk != f_last_clk);
+			f_last_clk <= i_clk;
+		end
+	end endgenerate
+
+`define	ASSUME	assume
 `else
-`define	`ASSUME	assert
+`define	ASSUME	assert
 `endif
 
 	reg	f_past_valid;
 	initial	f_past_valid = 1'b0;
-	always @($global_clock)
+	always @(posedge i_clk)
 		f_past_valid <= 1'b1;
 
-	initial	assume(!i_a_cyc);
-	initial	assume(!i_a_stb);
+	initial	`ASSUME(!i_a_cyc);
+	initial	`ASSUME(!i_a_stb);
 
-	initial	assume(!i_b_cyc);
-	initial	assume(!i_b_stb);
+	initial	`ASSUME(!i_b_cyc);
+	initial	`ASSUME(!i_b_stb);
 
-	initial	assume(!i_ack);
-	initial	assume(!i_err);
+	initial	`ASSUME(!i_ack);
+	initial	`ASSUME(!i_err);
+
+	always @(*)
+	if (!f_past_valid)
+		`ASSUME(i_reset);
 
 	always @(posedge i_clk)
 	begin
@@ -235,16 +259,13 @@ module	wbarbiter(i_clk, i_reset,
 			assert($past(r_a_owner) == r_a_owner);
 	end
 
-	wire	[(F_LGDEPTH-1):0]	f_nreqs, f_nacks, f_outstanding,
-			f_a_nreqs, f_a_nacks, f_a_outstanding,
-			f_b_nreqs, f_b_nacks, f_b_outstanding;
-
 	fwb_master #(.DW(DW), .AW(AW),
-			.F_MAX_STALL(0),
+			.F_MAX_STALL(F_MAX_STALL),
 			.F_LGDEPTH(F_LGDEPTH),
-			.F_MAX_ACK_DELAY(0),
+			.F_MAX_ACK_DELAY(F_MAX_ACK_DELAY),
 			.F_OPT_RMW_BUS_OPTION(1),
-			.F_OPT_DISCONTINUOUS(1))
+			.F_OPT_DISCONTINUOUS(1),
+			.F_OPT_CLK2FFLOGIC(F_OPT_CLK2FFLOGIC))
 		f_wbm(i_clk, i_reset,
 			o_cyc, o_stb, o_we, o_adr, o_dat, o_sel,
 			i_ack, i_stall, 32'h0, i_err,
@@ -255,7 +276,8 @@ module	wbarbiter(i_clk, i_reset,
 			.F_LGDEPTH(F_LGDEPTH),
 			.F_MAX_ACK_DELAY(0),
 			.F_OPT_RMW_BUS_OPTION(1),
-			.F_OPT_DISCONTINUOUS(1))
+			.F_OPT_DISCONTINUOUS(1),
+			.F_OPT_CLK2FFLOGIC(F_OPT_CLK2FFLOGIC))
 		f_wba(i_clk, i_reset,
 			i_a_cyc, i_a_stb, i_a_we, i_a_adr, i_a_dat, i_a_sel, 
 			o_a_ack, o_a_stall, 32'h0, o_a_err,
@@ -266,7 +288,8 @@ module	wbarbiter(i_clk, i_reset,
 			.F_LGDEPTH(F_LGDEPTH),
 			.F_MAX_ACK_DELAY(0),
 			.F_OPT_RMW_BUS_OPTION(1),
-			.F_OPT_DISCONTINUOUS(1))
+			.F_OPT_DISCONTINUOUS(1),
+			.F_OPT_CLK2FFLOGIC(F_OPT_CLK2FFLOGIC))
 		f_wbb(i_clk, i_reset,
 			i_b_cyc, i_b_stb, i_b_we, i_b_adr, i_b_dat, i_b_sel,
 			o_b_ack, o_b_stall, 32'h0, o_b_err,
@@ -292,6 +315,10 @@ module	wbarbiter(i_clk, i_reset,
 	if ((f_past_valid)&&(!$past(i_reset))
 			&&(!$past(i_a_cyc))&&($past(i_b_stb)))
 		assert(!r_a_owner);
+
+	always @(posedge i_clk)
+		if ((f_past_valid)&&(r_a_owner != $past(r_a_owner)))
+			assert(!$past(o_cyc));
 
 `endif
 endmodule
