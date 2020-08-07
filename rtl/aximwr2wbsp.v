3d1bc5d8e72f3be2a769ba52de5b19cc17aa7fc8
Tomasz Motylewski <tomek.dnl@gmail.com>
2020-08-07 12:30:32 +0200

    propagated AXI_LSBS to submodules because I need to override it

--- rtl/aximwr2wbsp.v
+++ rtl/aximwr2wbsp.v
@@ -42,7 +42,7 @@ module aximwr2wbsp #(
 	parameter C_AXI_DATA_WIDTH	= 32,// Width of the AXI R&W data
 	parameter C_AXI_ADDR_WIDTH	= 28,	// AXI Address width
 	parameter [0:0] OPT_SWAP_ENDIANNESS = 1'b1, // Lil to Big Endian swap
-	localparam AXI_LSBS		= $clog2(C_AXI_DATA_WIDTH)-3,
+	parameter AXI_LSBS		= $clog2(C_AXI_DATA_WIDTH)-3,
 	localparam AW			= C_AXI_ADDR_WIDTH-AXI_LSBS,
 	localparam DW			= C_AXI_DATA_WIDTH,
 
