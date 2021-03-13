079980da5810eee0d74e8d9d33095f8183b465e3
ZipCPU <dgisselq@ieee.org>
2021-03-13 09:08:26 -0500

    AXIVCAMERA: Removed an unused localparam

--- rtl/axivcamera.v
+++ rtl/axivcamera.v
@@ -255,8 +255,6 @@ module	axivcamera #(
 
 	localparam	LGMAXBURST = (TMPLGMAXBURST+ADDRLSB > 12)
 				? (12-ADDRLSB) : TMPLGMAXBURST;
-
-	localparam [ADDRLSB-1:0] LSBZEROS = 0;
 	// }}}
 
 	wire	i_clk   =  S_AXI_ACLK;
